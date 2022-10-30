2a54fdc315a1dce4e00c9525d371b6ff8c06be7c
qtqqtq <3101438240@qq.com>
2022-10-30 11:50:06 +0800

    修改了写入逻辑，比以前好一些

--- pythonProject/ss13翻译/读取.py
+++ pythonProject/ss13翻译/读取.py
@@ -7,39 +7,46 @@ file_info=[
         'folder_path':'D:\skyrat\Skyrat-tg-master\code\game\objects\items',
         're_list':['(?<=name = ".improper ).*(?=")','(?<=name = ".proper ).*(?=")','(?<=name = ").*(?=")','(?<=desc = ").*(?=")'],
         'file_name':'text.csv',
-        'filetree_name':'file_tree.csv'
+        'filetree_name':'file_tree.csv',
+        'id':'True'
     }
     ,
     {
         'folder_path': 'D:\skyrat\Skyrat-tg-master\code\game\objects\structures',
         're_list': ['(?<=name = ".improper ).*(?=")', '(?<=name = ".proper ).*(?=")', '(?<=name = ").*(?=")','(?<=desc = ").*(?=")'],
         'file_name': 'structures_text.csv',
-        'filetree_name': 'structures_file_tree.csv'
+        'filetree_name': 'structures_file_tree.csv',
+        'id':'True'
     }
     ,
     {
         'folder_path': 'D:\skyrat\Skyrat-tg-master\code\game\objects\effects',
         're_list': ['(?<=name = ".improper ).*(?=")', '(?<=name = ".proper ).*(?=")', '(?<=name = ").*(?=")','(?<=desc = ").*(?=")'],
         'file_name': 'effects_text.csv',
-        'filetree_name': 'effects_file_tree.csv'
+        'filetree_name': 'effects_file_tree.csv',
+        'id':'True'
     }
     ,
     {
         'folder_path':r'D:\skyrat\Skyrat-tg-master\tgui\packages\tgui\interfaces',
         're_list': ['(?<=title=").*(?=")','(?<=content=").*(?=")'],
         'file_name':'interface_text.csv',
-        'filetree_name':'interface_file_tree.csv'
+        'filetree_name':'interface_file_tree.csv',
+        'id':'False'
     }
 ]
 
 #返回[匹配内容，正则表达式，匹配物品]
-def search_file(file_path,re_list):
+def search_file(file_path,re_list,id_tf):
     f=open(file_path,'r',encoding='UTF-8')
-    stat=os.stat(file_path)
     result=[]
-    id=''
+    tmp_string=''
+    #用来检查一个id是否已经存在,仅在id不存在时生效
+    id_check_list=[]
     for string in f.readlines():
-        if list(string)[0]=='/':
+        if id_tf=='False':
+            id=re.match('.*',tmp_string).group()
+        if list(string)[0]=='/' and id_tf=='True':
             id=re.match('.*',string).group()
         for r_e in re_list:
             matchre=re.compile(r_e)
@@ -47,18 +54,22 @@ def search_file(file_path,re_list):
             if match!=None and match not in result:
                 result.append([match.group(0),r_e,id])
                 break
+        #传递到下一次迭代
+        if string not in id_check_list:
+            tmp_string=string
+            id_check_list.append(string)
     f.close()
     return result
 
-def search_folder(folder_path,re_list):
+def search_folder(folder_path,re_list,id_tf):
     result={}
     for root,dirs,files in os.walk(folder_path):
         for file_name in files:
-            result.update({root+'\\'+file_name:search_file(root+'\\'+file_name,re_list)})
+            result.update({root+'\\'+file_name:search_file(root+'\\'+file_name,re_list,id_tf)})
     return result
 
 #f_info[匹配内容，正则表达式，匹配物品]
-def write_file(file_path,file_tree):
+def write_file(file_path,file_tree,id_tf):
     global count
     try:
         f = open(file_path, 'r', encoding='UTF-8')
@@ -82,16 +93,18 @@ def write_file(file_path,file_tree):
                 old_file_line=list(change_f[line])
                 if re.match('.*', change_f[line]).group() == id:
                     switch=True
-                elif change_f[line][0]=='/':
+                #按id写入
+                elif change_f[line][0]=='/' and id_tf=='True':
                     switch=False
-                if id == '':
-                    switch=True
                 #找到了写入的位置，开始按顺序写入正则表达式对应内容
                 if switch:
                     match=re.search(r_e,change_f[line])
                     if match!=None:
                         old_file_line=old_file_line[:match.span()[0]]+list(text)+old_file_line[match.span()[1]:]
                         count+=1
+                        #不按id写入，按上一文本写入，写入完后就终止
+                        if id_tf=='False':
+                            switch=False
                         try:
                             text, r_e = next(text_iter)
                         except StopIteration:
@@ -106,8 +119,8 @@ def write_file(file_path,file_tree):
         fw.write(line)
     fw.close()
 
-def read_folder(folder_path,filetree_name,file_name,re_list):
-    file_tree = search_folder(folder_path,re_list)
+def read_folder(folder_path,filetree_name,file_name,re_list,id_tf):
+    file_tree = search_folder(folder_path,re_list,id_tf)
     with open(filetree_name, 'w', newline='', encoding='UTF-8') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['文件路径', '正则表达式'])
@@ -122,7 +135,7 @@ def read_folder(folder_path,filetree_name,file_name,re_list):
             for line in file_tree[file]:
                 writer.writerow([next(index), line[0], ''])
 
-def write_tree(file_name,filetree_name):
+def write_tree(file_name,filetree_name,id_tf):
     with open(filetree_name, 'r', newline='', encoding='UTF-8') as csvfile:
         tree_reader = csv.reader(csvfile)
         with open(file_name, 'r', newline='', encoding='UTF-8') as text:
@@ -144,7 +157,7 @@ def write_tree(file_name,filetree_name):
                     new_file_tree[i[0][0]] = [[new_text, i[0][1], i[0][2]]]
 
     for file_path in new_file_tree.keys():
-        write_file(file_path, new_file_tree)
+        write_file(file_path, new_file_tree,id_tf)
 
 #储存结构{绝对路径：【【文本内容，正则表达式】，【。。。】，，。。。。】，绝对路径：【。。。】}
 if __name__=='__main__':
@@ -154,9 +167,9 @@ if __name__=='__main__':
         print('操作中')
         if enter=='r':
             for info in file_info:
-                read_folder(info['folder_path'],info['filetree_name'],info['file_name'],info['re_list'])
+                read_folder(info['folder_path'],info['filetree_name'],info['file_name'],info['re_list'],info['id'])
         elif enter=='w':
-            count=0
             for info in file_info:
-                write_tree(info['file_name'],info['filetree_name'])
-            print(count)
+                count=0
+                write_tree(info['file_name'],info['filetree_name'],info['id'])
+                print(info['file_name']+'已写入'+str(count)+'个词条')
